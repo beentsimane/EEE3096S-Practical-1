9496d10b03e54d495b81a497b4ddf1956812d273
AnesuMugiya <71221500+AnesuMugiya@users.noreply.github.com>
2024-08-21 14:53:21 +0200

    Start TIM2 in Output Compare mode on channel 1

--- Prac 2/main.c
+++ Prac 2/main.c
@@ -111,7 +111,8 @@ int main(void)
 HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
 
   // TODO: Start TIM2 in Output Compare (OC) mode on channel 1.
-
+HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_1);
+__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, TIM2_Ticks);
 
   // TODO: Start DMA in IT mode on TIM2->CH1; Source is LUT and Dest is TIM3->CCR3; start with Sine LUT
 
